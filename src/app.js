8efd50ea89fe1282782c4d5eb6ac347b9ea3c047
mellyssaStephanny <mellyssa.smendes@gmail.com>
2022-12-17 05:07:09 -0300

    :wrench: adiciona swagger ao projetr

--- src/app.js
+++ src/app.js
@@ -5,12 +5,16 @@ const cors = require('cors')
 const mongoose = require('./database/mongooseConnect')
 const donorRoutes = require('./routes/donorRoutes')
 const institutionRoutes = require('./routes/institutionRoutes')
+const swaggerUi = require('swagger-ui-express')
+const swaggerFile = require('../swagger/swagger_output.json')
 const app = express()
 
 app.use(express.json())
 app.use(cors())
+
 mongoose.connect()
 
+app.use('/documentacao', swaggerUi.serve, swaggerUi.setup(swaggerFile))
 app.use('/reprograma-praquemdoar/donor', donorRoutes)
 app.use('/reprograma-praquemdoar/institution', institutionRoutes)
 
