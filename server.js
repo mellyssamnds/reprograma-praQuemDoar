8ac42f651978aefbec04bedfcc1839f5a23afb8a
mellyssaStephanny <mellyssa.smendes@gmail.com>
2022-12-17 05:48:48 -0300

    :wrench: adiciona rota raiz do projeto

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const app = require('./src/app')
 const PORT = process.env.PORT
 
+app.get('/', (req, res) => {
+  res.send({ message: 'Bem-vindos ao Portal Pra Quem Doar!' })
+})
+
 app.listen(PORT, () => {
   console.log(`API running in Port ${PORT}`)
 })
